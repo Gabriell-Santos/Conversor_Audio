22483b753b1bb8b1c0e94c5bbfee3a386dce25eb
Gabriel <ferreirag.santos.dev@gmail.com>
2025-01-16 11:32:38 -0300

    criando a funcionalidade de leitura

--- Script.js
+++ Script.js
@@ -3,3 +3,11 @@
 let TextArea = document.querySelector("#textarea");
 let Voices = document.querySelector("voices");
 let Button = document.querySelector("button");
+
+// Criado a Função de Verificação do Texto!
+Button.addEventListener("click", () => {
+  if (TextArea.value !== "") {
+    let speak = new SpeechSynthesisUtterance(TextArea.value);
+    window.speechSynthesis.speak(speak);
+  }
+});
