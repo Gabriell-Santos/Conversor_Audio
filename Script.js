9b51364b7121dbccebb911af8036ddb15e5ff9fc
Gabriel <ferreirag.santos.dev@gmail.com>
2025-01-16 11:47:50 -0300

    colocando no select a lista de vozes

--- Script.js
+++ Script.js
@@ -4,6 +4,16 @@ let TextArea = document.querySelector("#textarea");
 let Voices = document.querySelector("voices");
 let Button = document.querySelector("button");
 
+// Criando a lista de Vozes!
+window.speechSynthesis.addEventListener("voiceschanged", () => {
+  let voicesList = window.speechSynthesis.getVoices();
+  for (let V in voicesList) {
+    let optionEL = document.createElement("option");
+    optionEL.setAttribute("value", V);
+    optionEL.innerHTML = voicesList[V];
+  }
+});
+
 // Criado a Função de Verificação do Texto!
 Button.addEventListener("click", () => {
   if (TextArea.value !== "") {
